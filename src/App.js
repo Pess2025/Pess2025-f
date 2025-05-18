cc58bbd5653cdf812969efbd1bf75897c35fdd73
Yeojin0523 <d02jyjeong@naver.com>
2025-05-18 20:58:32 +0900

    Style : main 화면 jsx와 css 작성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import TopHeader from "./Common/TopHeader/TopHeader";
-import Main from './Pages/Main/Main.jsx';
+import Main from "./Pages/Main/Main";
 import {ROUTES} from "./Common/Routes";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <TopHeader>
-      <Routes>
-        <Route path={ROUTES.MAIN} element={<Main/>}/>
-      </Routes>
-    </TopHeader>
+      <Router>
+        <TopHeader/>
+        <Routes>
+            <Route path={ROUTES.MAIN} element={<Main/>}/>
+        </Routes>
+      </Router>
   );
 }
 
