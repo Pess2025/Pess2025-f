25adfb91a6fe1f8e12be4685d515882cc36c3a21
CHAEMIN-0 <41.31.cm@gmail.com>
2025-05-19 16:10:08 +0900

    사이드 바 수정 중, 1 인덱스 오류

--- src/Pages/Decode/decode.jsx
+++ src/Pages/Decode/decode.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import TopHeader from '../../Common/TopHeader/TopHeader';
 import styles from './Decode.module.css';
-import decodeImage from './assets/Decode.png';
+import uploadIcon from './assets/upload_icon.png';
 
 export default function Decode() {
   const [fileName, setFileName] = useState(null);
@@ -22,14 +22,57 @@ export default function Decode() {
 
   return (
     <div className={styles.container}>
-      <TopHeader />
       <div className={styles.body}>
         <aside className={styles.sidebar}>
-          <ol className={styles.steps}>
-            <li className={styles.active}><span>1</span> 암호화 된 파일 업로드<ul><li>파일 업로드</li></ul></li>
-            <li><span>2</span> 복호화 준비</li>
-            <li><span>3</span> 복호화 수행</li>
-          </ol>
+          <ul className={styles.steps}>
+            <div className = {styles.onrBlock}>
+                <li className={styles.active}>
+                    <div className={styles.stepRow}>
+                    <div className={styles.stepItem}>
+                        <span>1</span> 
+                        <div className={styles.text}>
+                            암호화 된 파일 업로드
+                            <ul>
+                                <li className={styles.active_mini}>파일 업로드</li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                </li>
+            </div>
+            <li>
+                <div className={styles.stepRow}>
+                <div className={styles.stepItem}>
+                    <span>2</span>
+                    <div className={styles.text}>복호화 준비</div>
+                </div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.stepRow}>
+                <div className={styles.stepItem}>
+                    <span>3</span>
+                    <div className={styles.text}>복호화 수행</div>
+                </div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.stepRow}>
+                <div className={styles.stepItem}>
+                    <span>4</span>
+                    <div className={styles.text}>파일 무결성/전자서명 검증</div>
+                </div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.stepRow}>
+                <div className={styles.stepItem}>
+                    <span>5</span>
+                    <div className={styles.text}>키 검색 및 원본 다운로드</div>
+                </div>
+                </div>
+            </li>
+          </ul>
         </aside>
 
         <main className={styles.main}>
@@ -48,7 +91,7 @@ export default function Decode() {
               ) : (
                 <>
                   <p>파일을 드래그 해서 올려주세요.</p>
-                  <img src={decodeImage} alt="upload" className={styles.uploadIcon} />
+                  <img src={uploadIcon} alt="upload" className={styles.uploadIcon} />
                 </>
               )}
               <input type="file" className={styles.inputHidden} onChange={handleFileChange} />
