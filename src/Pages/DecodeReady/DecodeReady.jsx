09cc620d9c9d4d5368bc18e6af1f9e8a7bff1f3a
CHAEMIN-0 <41.31.cm@gmail.com>
2025-06-03 02:06:36 +0900

    파일 확인 까지 완료

--- src/Pages/DecodeReady/DecodeReady.jsx
+++ src/Pages/DecodeReady/DecodeReady.jsx
@@ -1,20 +1,41 @@
-import React, { useState, useRef } from 'react';
+import React, { useState,useEffect, useRef } from 'react';
 import styles from './DecodeReady.module.css';
 import checkImg from './assets/check.png';
 import loadingImg from './assets/loading.png';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../Common/Routes';
+import axios from 'axios';
 
 export default function DecodeReady() {
   const [symmetricDone, setSymmetricDone] = useState(false);
+  const [error, setError] = useState('');
+  const [loadingDone, setLoadingDone] = useState(false);
+
   const navigate = useNavigate();
 
-    // 테스트용 타이머 -> 시간에 따라 상태 보여줌 / 백엔드 개발 후에는 타이머 말고 값 리턴 확인하고 바뀌도록 변경 해야함
-    React.useEffect(() => {
-      const symTimer = setTimeout(() => setSymmetricDone(true), 2000);
+ //사용자가 수행 중인지 알 수 있게 타이머 넣어 둠
+  React.useEffect(() => {
+      const symTimer = setTimeout(() => setLoadingDone(true), 1000);
       return () => {
         clearTimeout(symTimer);
       };
+    }, []);
+
+    useEffect(() => {
+      axios.get("http://localhost:8080/decode/check-files")
+         .then(response => {
+              const files = response.data;
+              const allExist = files["password.txt"] && files["password_hash.txt"] && files["envelope.key"] && files["public.key"];
+              if (allExist) {
+                setSymmetricDone(true);
+              } else {
+                setError("필요한 파일이 존재 하지 않습니다.");
+              }
+            })
+        .catch(err => {
+          console.error(err);
+          setError("오류입니다.");
+        });
     }, []);
 
   return (
@@ -73,12 +94,18 @@ export default function DecodeReady() {
           <div className={styles.block}>
               <div className={styles.blockHeader}>서버에서 복호화에 사용할 파일 확인</div>
               <div className={styles.blockBody}>
-                <p>{symmetricDone ? '대상 파일을 확인 했습니다. 복호화가 가능 합니다.' : '파일을 찾는 중 입니다.'}</p>
-                <img src={symmetricDone ? checkImg : loadingImg} alt="상태 이미지" className={styles.statusIcon} />
+                  {loadingDone && error ? (
+                            <p style={{ color: 'red' }}>{error}</p>
+                  ) : (
+                    <>
+                        <p>{loadingDone && symmetricDone ? '대상 파일을 확인 했습니다. 복호화가 가능 합니다.' : '파일을 찾는 중 입니다.'}</p>
+                        <img src={loadingDone && symmetricDone ? checkImg : loadingImg} alt="상태 이미지" className={styles.statusIcon} />
+                    </>
+                  )}
               </div>
             </div>
             {/* 완료되었을 때만 다음 버튼 */}
-              {symmetricDone && (
+              {symmetricDone && loadingDone && (
                 <div className={styles.buttons}>
                   <button className={styles.primary} onClick={() => navigate(ROUTES.DECODE_DO)}>다음</button>
                 </div>
